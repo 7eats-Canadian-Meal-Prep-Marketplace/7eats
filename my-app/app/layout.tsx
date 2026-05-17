6b9f7746ced5dc909b3fc82b0ac1a50470438bf1
Moe1177 <maddasi04@gmail.com>
2026-05-17 01:45:57 -0400

    feat: add Sonner Toaster with app design system styling

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import Script from "next/script";
+import { Toaster } from "sonner";
 import CalendlyBadge from "@/app/components/CalendlyBadge";
 import BackToTop from "@/app/components/BackToTop";
 import "./globals.css";
@@ -38,6 +39,12 @@ export default function RootLayout({
         {children}
         <CalendlyBadge />
         <BackToTop />
+        <Toaster
+          position="top-center"
+          expand={false}
+          richColors={false}
+          closeButton
+        />
         <Script
           src="https://assets.calendly.com/assets/external/widget.js"
           strategy="afterInteractive"
